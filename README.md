b95238a0362a9ddd06b4f4181916baaf7ea9acfe
Dharmesh53 <dharmeshwr@gmail.com>
2025-02-08 18:55:46 +0530

    feat: add readme

---
+++ README.md
@@ -0,0 +1,40 @@
+Got tired of setting up ESLint, Prettier, Nodemon, and TypeScript repeatedly.So I created this handy script to simplify the process.
+
+Feel free to use it however you like! If you'd like to use it my way:
+
+Just install the script, simply run:
+```bash
+./installer.sh
+```
+What it does ??
+- Copies the `installer.sh` script to `~/.local/bin`.
+
+- Copies the config folder to `~/.local/share`.
+
+Ensure `~/.local/bin` is included in your PATH environment variable.
+
+Add this line to your shell configuration file (e.g., `~/.bashrc` or `~/.zshrc`):
+```bash
+export PATH="/home/{username}/.local/bin:$PATH"
+```
+
+Replace {username} with your actual username. After saving, reload your shell configuration.
+
+Once set up, you can use the script from anywhere like this:
+```bash
+setup-typescript-app express-app
+```
+
+It will create a directory `express-app` where you called the script, other than that you can give it any other path.
+```bash
+setup-typescript-app ~/Workspace/express-app
+```
+
+You can also use `--quiet` or `-q` to hide the logs.
+```bash
+setup-typescript-app ~/Workspace/express-app -q
+```
+
+
+## For now the installer will only work on LINUX.
+
--- installer.sh
+++ installer.sh
@@ -1,55 +1,6 @@
 #!/bin/bash
 
-log() {
-  if [[ $QUIET == false ]]; then
-    printf "$1"
-  fi
-}
-
-if [ -z "$1" ]; then
-  printf 'No directory provided\n'
-  printf "Usage: $(basename $0) <directory>\n"
-  exit 1
-fi
-
-printf 'This will create a app with eslint, prettier, nodemon and typescript setup\n' | lolcat
-
-QUIET=false
-CONF_DIR_PATH=$(realpath 'config')
-DEST_DIR_PATH=$(realpath $1)
-
-if [[ $2 == '-q' || $2 == '--quiet' ]]; then
-  QUIET=true
-fi
-
-if [ ! -d "$DEST_DIR_PATH" ]; then 
-  printf "# Creating directory\n" | lolcat
-  mkdir -p $DEST_DIR_PATH
-fi
-
-
-printf "# Coping all config files in your app \n" | lolcat
-for file in "$CONF_DIR_PATH"/* "$CONF_DIR_PATH"/.*; do
-
-  filename=$(basename "$file")
-  log "Coping $filename\n"
-
-  dest_file="$DEST_DIR_PATH/$filename"
-
-  if [ -e "$dest_file" ]; then
-    backup_file="$dest_file-backup"
-    mv $dest_file $backup_file
-  fi
-
-  cp -r $file $DEST_DIR_PATH
-done
-
-printf "# Installing packages\n" | lolcat
-if [[ $QUIET == false ]]; then
-  (cd $DEST_DIR_PATH && pnpm install)
-else
-  (cd $DEST_DIR_PATH && pnpm install 2>/dev/null 1>/dev/null)
-fi
-
-printf "# Running the app\n" | lolcat
-(cd $DEST_DIR_PATH && pnpm start)
+mkdir -p ~/.local/share/setup-typescript-app
+cp -r config ~/.local/share/setup-typescript-app
+mkdir -p ~/.local/bin
+cp setup-typescript-app ~/.local/bin
